b27774bb4df147dc25b95a51a86684e26d19c493
aheadwhat <chengh37@mail2.sysu.edu.cn>
2015-09-28 10:44:47 +0800

    cash computation for R "solve"
    function used to cash the computation of the inverse of a matirx

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
+## The functions provided here can cash the inverse of 
+## a matrix, which is usually a costly computation
 
-## Write a short comment describing this function
-
+## "makeCacheMatrix" is used to set and get the value 
+## of a matrix and its inverse respectively
+#  test matrix:x<-matrix(c(1,2,3,4),nrow=2)
 makeCacheMatrix <- function(x = matrix()) {
-
+  i<-NULL
+  set<-function(y){
+    x<<-y
+    i<<-NULL
+  }
+  get<-function() x
+  setinv<-function(inv) i<<-inv
+  getinv<-function() i
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+## "cacheSolve" is used to calculates the inverse of the matix
+## created by the above function
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  i<-x$getinv()
+  if(!is.null(i)){
+    message("getting cached data")
+    return(i)
+  }
+  data<-x$get()
+  i<-solve(data,...)
+  x$setinv(i)
+  i
 }
+
+# verify the result:cacheSolve(makeCacheMatrix(x))
+ 
